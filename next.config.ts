9034e0a1b9d74bf45b3d11f7409297753542702e
Henrique <henriquesouza432@outlook.com>
2025-05-07 14:50:12 -0300

    feat: enable unoptimized images in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         hostname: 'arena-pro.s3.us-east-2.amazonaws.com',
